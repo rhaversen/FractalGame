c1e58662577b41e8347bf548fe354a56e5b28d88
rhaversen <rhaversen@gmail.com>
2025-09-06 17:49:15 +0700

    initial committ

---
+++ Source/Fractal/Fractal.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Fractal.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Fractal, "Fractal" );
